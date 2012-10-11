4f7260025084d7f8292dca6c9b20156acf2ed9cf
Josh Fernandes <joshfernandes@mac.com>
2012-10-10 21:58:24 -0700

    changed information to a list

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World and everyone!'
+    return [9.8,'m/s^2','standard gravity']
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
