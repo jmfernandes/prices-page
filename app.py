6fa08cd0470ac556914299980a017b1d6e96112b
Josh Fernandes <joshfernandes@mac.com>
2012-11-08 20:32:04 -0800

    jinja

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ def index():
 def hello(name=5):
     verb="like"
     aquire = urllib2.Request("http://www.prices.datanab.net/us/gasoline_json")
-    data = ast.literal_eval(aquire)
+    unpacked = aquire.read()
+    data = ast.literal_eval(unpacked)
     #opener = urllib2.build_opener()
     #results = opener.open(aquire)
     #final = results.read()
