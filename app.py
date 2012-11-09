88be275ebfafcd29c92217a442fe40fe0d809c47
Josh Fernandes <joshfernandes@mac.com>
2012-11-08 19:19:37 -0800

    jinja

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def index():
 def hello(name=5):
     verb="like"
     aquire = urllib2.Request("http://www.prices.datanab.net/us/gasoline_json")
-    f = urllib2.urlopen(aquire)
     return render_template('gasoline.html',name=name, verb=verb)
 
 @app.route('/us/gasoline_json', endpoint='gasoline_json')
