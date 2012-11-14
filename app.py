11f5d1b991ce0b088f2587cbd11f776491146689
Josh Fernandes <joshfernandes@mac.com>
2012-11-13 17:04:15 -0800

    trying to get json to work

--- app.py
+++ app.py
@@ -21,24 +21,32 @@ def index():
 def hello(name=5):
     verb="like"
     aquire = urllib2.Request("http://www.prices.datanab.net/us/gasoline_json")
-    dict = {
-    "value": 3.492,
-    "units": "dollars/gallon",
-    "name": "Average Price of Gasoline in United States",
-    "citation": "http://www.eia.gov/petroleum/gasdiesel/"
-    }
+    response = urllib2.urlopen(aquire)
+    the_page = response.read()
+    data = json.loads(the_page)
+    #aquire2 = str(aquire)
+    #unpacked = json.loads(aquire2)
+    #dict = {
+    #"value": 3.492,
+    #"units": "dollars/gallon",
+    #"name": "Average Price of Gasoline in United States",
+    #"citation": "http://www.eia.gov/petroleum/gasdiesel/"
+    #}
     #unpacked = aquire.read()
     #data = ast.literal_eval(unpacked)
     #opener = urllib2.build_opener()
     #results = opener.open(aquire)
     #final = results.read()
     #hey = urllib2.urlopen(aquire)
-    return render_template('gasoline.html',name=name, data=dict)
+    return render_template('gasoline.html',name=name, data=data)
 
 @app.route('/us/gasoline_json', endpoint='gasoline_json')
 def index():
     return render_template('json/gasoline.json')
 
+#app.debug = True
+#app.run()
+#exit()
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
