13e45a53b9be1e9436c056b24cd6ceb579b6d967
Josh Fernandes <joshfernandes@mac.com>
2012-11-14 10:51:00 -0800

    working on json

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def index():
 @app.route('/us/gasoline', endpoint='gasoline')
 def index():
     req = urllib2.Request("http://www.prices.datanab.net/us/gasoline_json")
-    yo = ast.literal_eval(req)
+    yo = ast.literal_eval("http://www.prices.datanab.net/us/gasoline_json")
     #response = urllib2.urlopen(req)
     #data2 = response.read()
     #data = json.loads(data2)
