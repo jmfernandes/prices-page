c8c089dc0a96b62ff85517449c126bf425be9d6a
Josh Fernandes <joshfernandes@mac.com>
2012-11-14 15:54:22 -0800

    json

--- testing.py
+++ testing.py
@@ -1,4 +1,15 @@
 import json
+import urllib
+aquire = urllib.urlopen("http://www.constants.datanab.net/physics/planck_constant_json")
+unpacked = aquire.read()
+data = json.loads(unpacked)
+
+print data["value"]
+
+
+
+exit()
+import json
 
 json_file = open('templates/json/gasoline.json')
 data = json.load(json_file)
