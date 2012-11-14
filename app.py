30d270fdc7d8d55593ca1de13a8bc9cb6107f7ad
Josh Fernandes <joshfernandes@mac.com>
2012-11-13 17:08:18 -0800

    trying to get json to work

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ from flask import render_template
 
 app = Flask(__name__)
 
+#class Number(object):
+#    def __init__(self):
+#        self.aquire = urllib.urlopen("http://www.prices.datanab.net/us/gasoline_json")
+#        self.unpacked = self.aquire.read()
+#        self.data = json.loads(self.unpacked)
 
 @app.route('/')
 def index():
@@ -18,13 +23,30 @@ def hello(name=5):
     aquire = urllib2.Request("http://www.prices.datanab.net/us/gasoline_json")
     response = urllib2.urlopen(aquire)
     the_page = response.read()
-    dict = json.loads(the_page)
+    #data = json.loads(the_page)
+    #aquire2 = str(aquire)
+    #unpacked = json.loads(aquire2)
+    dict = {
+    "value": 3.492,
+    "units": "dollars/gallon",
+    "name": "Average Price of Gasoline in United States",
+    "citation": "http://www.eia.gov/petroleum/gasdiesel/"
+    }
+    #unpacked = aquire.read()
+    #data = ast.literal_eval(unpacked)
+    #opener = urllib2.build_opener()
+    #results = opener.open(aquire)
+    #final = results.read()
+    #hey = urllib2.urlopen(aquire)
     return render_template('gasoline.html',name=name, data=dict)
 
 @app.route('/us/gasoline_json', endpoint='gasoline_json')
 def index():
     return render_template('json/gasoline.json')
 
+#app.debug = True
+#app.run()
+#exit()
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
