d1f43212862538a6cf45411ab5827cc51853c0c8
Josh Fernandes <joshfernandes@mac.com>
2017-01-15 20:27:53 -0800

    remmoved sl

--- app.py
+++ app.py
@@ -3,13 +3,6 @@ from flask import Flask, render_template, json
 
 app = Flask(__name__)
 
-@app.before_request
-def before_request():
-    if request.url.startswith('http://'):
-        url = request.url.replace('http://', 'https://', 1)
-        code = 301
-        return redirect(url, code=code)
-
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
